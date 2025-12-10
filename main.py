7117375f13f0cb7f7ff4a939e8951c1d9dcc9caa
Prajakta Patil <prajaktap816@gmail.com>
2025-12-10 22:27:35 +0530

    second commit with arithmetic python file

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import arithmetic
 
-print("this is the first python file ")
+num1 = int(input("enter num 1 = "))
+num2 = int(input("enter num 2 = "))
+
+arithmetic.addition(num1, num2)
+
+print("this is the first python file")
